c1e830c01dcddc905414e2ee372a02fdd8379136
bongjun <bongjun@corelab.or.kr>
2016-04-30 00:48:32 +0900

    UVA: fix ack

--- tools/uva/uva_manager.cpp
+++ tools/uva/uva_manager.cpp
@@ -503,6 +503,7 @@ namespace corelab {
 
         socket->receiveQue();
         int ack = socket->takeWordF();
+        printf("MEMSET: ack %d\n", ack);
         assert(ack == MEMSET_REQ_ACK && "wrong");
 
         LOG("[client] Memset : memsetHandler END (%p)\n\n", addr);
